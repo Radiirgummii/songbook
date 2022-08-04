d7a9c5eebd6135e3b16f3ea639165c12c3aa35a3
Radiirgummii <git@radiirgummii.net>
2022-08-04 23:59:03 +0200

    added custom indices

--- main.py
+++ main.py
@@ -56,38 +56,38 @@ def render_chord(txt):
         pdf.ln(fontsize*0.35)
 
 
-def add_song(data,title):
-    global pagenumbers, pictures , picnb
+def add_song(data, title):
+    global pagenumbers, pictures, picnb
 
-
-    #add page if song has 2 pages so you would have to scroll
+    # add page if song has 2 pages so you would have to scroll
     h = fontsize * 0.9
     for i in data[title]["scheme"]:
         h += data[title]["txt"][i].count("%") * fontsize * 0.6
         h += fontsize * 0.55
     if h >= 150 and pdf.page_no() % 2 == 0:
         pdf.add_page()
-        pdf.image(f"../pictures/{pictures[picnb]}" ,10,10,128,180) 
+        pdf.image(f"../pictures/{pictures[picnb]}", 10, 10, 128, 180)
         picnb += 1
-    
 
-    #add Title
+    # add Title
     pdf.add_page()
     pdf.set_font("times", "b", fontsize * 1.2)
     pdf.cell(40, fontsize, title)
     pdf.ln(fontsize*0.9)
     l = fontsize * 2
     pdf.set_font("Courier", "", fontsize)
-    a = {title : pdf.page_no()}
+    a = {title: pdf.page_no()}
     pagenumbers.update(a)
     h = fontsize * 0.9
     for i in data[title]["scheme"]:
-        if pdf.get_y() +(data[title]["txt"][i].count("%") * fontsize * 0.65) + 10 >= 190:
+        if pdf.get_y() + (data[title]["txt"][i].count("%") * fontsize * 0.65) + 10 >= 190:
             pdf.add_page()
-            h = data[title]["txt"][i].count("%") * fontsize * 0.6 + fontsize * 0.55
+            h = data[title]["txt"][i].count(
+                "%") * fontsize * 0.6 + fontsize * 0.55
         render_chord(data[title]["txt"][i])
         pdf.ln(fontsize*0.55)
     print(f'sucsessfully added song "{title}"')
+
 
 def create_index(index):
     pdf.add_page()
@@ -98,10 +98,11 @@ def create_index(index):
     l = fontsize * 2
     pdf.set_font("Courier", "", fontsize)
     for i in index.items():
-        
+
         pdf.cell(40, fontsize, f"{i[0]+' ' * (40-len(i[0]))}{i[1]}")
         pdf.ln(fontsize * 0.3)
     print("sucsessfully added index")
+
 
 # init vars
 fontsize = 9
@@ -110,23 +111,27 @@ pagenumbers = {}
 pictures = os.listdir("../pictures")
 picnb = 0
 
-#reading Data
+# reading Data
 with open("songs.json", 'r') as f:
     data = json.load(f)
-index = data["index"]
+print("please choose your index:")
+for i , name in zip(range(len(data["index"])),data["index"].keys()):
+    print(f"{i+1} : {name}")
+print(list(data["index"].keys()))
+index = data["index"][list(data["index"].keys())[int(input())-1]]
 
-#Add title page
+# Add title page
 pdf.add_page()
 pdf.set_xy(0, 100)
 pdf.set_font("times", "", 80)
-pdf.cell(148, 10, "Liederbuch",0,0,"C")
+pdf.cell(148, 10, "Liederbuch", 0, 0, "C")
 
-#Add songs
+# Add songs
 for i in index:
-    add_song(data,i)
+    add_song(data, i)
 
 create_index(pagenumbers)
 
-#Output pdf
+# Output pdf
 pdf.output('songbook.pdf', 'F')
 print('sucsessfully outputted songbook as "songbook.pdf"')

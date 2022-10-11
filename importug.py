002c3a66152bbe9a32ac9a4a1c511aba859e3faf
Radiirgummii <git@radiirgummii.net>
2022-10-10 23:44:13 -0700

    Added song "Riptide" improved ugimport.py

--- importug.py
+++ importug.py
@@ -1,15 +1,32 @@
+import json
+
 with open("tmp", 'r') as f:
     a = f.readlines()
-    b = []
-    for j, i in zip(a, range(0, len(a))):
-        b += [j.replace("\n", "")]
-    for i in range(0, len(b)-1, 2):
-        c = []
-        for j, k in enumerate(b[i]):
-            if not k == " ":
-                c += [(j, k)]
-        print(b[i+1][:c[0][0]], end="")
-        for j in range(len(c)):
-            print(b[i+1][c[j-1][0]:c[j][0]] +
-                  "{" + c[j][1] + "}", end="")
-        print(b[i+1][c[-1][0]:], end="%")
+    b = {}
+    l=0
+    s = False
+    song ={"scheme":[],"txt":{}}
+    for i,j in enumerate(a):
+        if j[0] =="+":
+            song["scheme"] += [j.replace("+","").replace("\n", "")] 
+        if s is True and j[0]!="#":
+            b[l] += [j.replace("\n", "")]
+        if j[0]=="#":
+            l = j.replace("#","").replace("\n", "")
+            b[l]=[]
+            s = True
+    for g,h in b.items():
+        song["txt"][g] = ""
+        for i in range(0,len(h), 2):
+            c = []
+            for j, k in enumerate(h[i]):
+                if not k == " ":
+                    c += [(j, k)]
+            
+            song["txt"][g] += h[i+1][:c[0][0]]
+            for j in range(len(c)):
+                song["txt"][g] += h[i+1][c[j-1][0]:c[j][0]] + "{" + c[j][1] + "}"
+            song["txt"][g]+= h[i+1][c[-1][0]:] + "\n"
+                
+    with open("tmp.json", "a") as file:
+        json.dump(song,file)
